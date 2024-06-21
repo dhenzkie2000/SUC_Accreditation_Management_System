978fe394c7a03a94dcca0eb510c44c4ef89cd46a
Dhenzkie <gindhen@gmail.com>
2024-06-21 16:47:52 +0800

    I made the db_connect accessable in live server.

--- connection/db_connect.php
+++ connection/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-    $connection = pg_connect("host=localhost port=5432 dbname=ACCSYS user=postgres password=dhenzkie2000");
+    $connection = pg_connect("host=el101finalrequirement.postgres.database.azure.com port=5432 dbname=ACCSYS user=el101 password=sucaccreditation@2024");
     
     if(!$connection){
         echo "Error";
